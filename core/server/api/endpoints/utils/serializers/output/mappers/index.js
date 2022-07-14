a1263b5cc66f86cc103a00ebe5634f97f9c2e229
Pavel Yemelianov <ipauler@gmail.com>
2022-07-14 17:11:07 +0200

    Init

---
+++ core/server/api/endpoints/utils/serializers/output/mappers/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    actions: require('./actions'),
+    authors: require('./authors'),
+    comments: require('./comments'),
+    emails: require('./emails'),
+    images: require('./images'),
+    integrations: require('./integrations'),
+    pages: require('./pages'),
+    posts: require('./posts'),
+    settings: require('./settings'),
+    snippets: require('./snippets'),
+    tags: require('./tags'),
+    newsletters: require('./newsletters'),
+    users: require('./users')
+};
